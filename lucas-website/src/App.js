cfea736fe81fde6de9df1010d90a4aa689b366f7
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-05-04 22:09:35 +0200

    Add navbar and theme switch

--- lucas-website/src/App.js
+++ lucas-website/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className="App">
+        <Navbar data-bs-theme={theme} expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand href="#home">Hey</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">About me</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+          {/* Put the container to the right */}
+          <Container className="d-flex justify-content-end">
+            <div className={`theme-switch ${theme}`}>
+              <input
+                onChange={toggleTheme} 
+                checked={theme === 'dark'}
+                type="checkbox"
+                id="custom-switch"
+              />
+              <label htmlFor="custom-switch"></label>
+            </div>
+          </Container>
+        </Navbar>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
