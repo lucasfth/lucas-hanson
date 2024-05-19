47261c95add8dcd811e3ac1dd1a12d90f928e51f
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-05-19 21:21:24 +0200

    Fix easter egg interval

--- lucas-website/src/intro/Intro.jsx
+++ lucas-website/src/intro/Intro.jsx
@@ -1,11 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, useRef } from 'react';
 import { ThemeContext, NavbarHeightContext } from '../App';
 import lucas_full_body from './assets/lucas_full_body.webp';
 import wet_lucas from './assets/wet_lucas.webp';
 import './intro.css';
 import Row from 'react-bootstrap/Row';
+import { Link as ScrollLink } from 'react-scroll';
 
 function Intro() {
   const { theme } = useContext(ThemeContext);
@@ -17,18 +18,27 @@ function Intro() {
   const navbarHeight = useContext(NavbarHeightContext);
 
   const [isFlipped, setIsFlipped] = useState(false);
-
+  const [wiggle, setWiggle] = useState(false);
+  const isWiggling = useRef(false);
+  
   const handleClick = () => {
-    setIsFlipped(!isFlipped);
+    isWiggling.current = false;
+    setWiggle(false);
+    setTimeout(() => {
+      setIsFlipped(!isFlipped);
+    }, 0);
   };
-
-  const [isWiggling, setIsWiggling] = useState(false);
-
+  
   useEffect(() => {
     const interval = setInterval(() => {
-      setIsWiggling(prevIsWiggling => isFlipped ? false : !prevIsWiggling);
+      isWiggling.current = true;
+      setWiggle(true);
+      setTimeout(() => {
+        isWiggling.current = false;
+        setWiggle(false);
+      }, 1000);
     }, 5000);
-
+  
     return () => clearInterval(interval);
   }, [isFlipped]);
 
@@ -36,7 +46,7 @@ function Intro() {
     <>
       <Row>
         <Container style={{ textAlign: 'left', paddingTop: `${navbarHeight}px` }}>
-          <div className={`image-container ${isWiggling ? 'wiggle' : ''}`}
+          <div className={`image-container ${wiggle ? 'wiggle' : ''}`}
           style={{
             maxWidth: window.innerWidth <= 750 ? '100%' : '20%',
             width: window.innerWidth <= 750 ? '100%' : '20%',
@@ -49,14 +59,14 @@ function Intro() {
             transition: 'transform 0.8s',
           }}
           onClick={handleClick}>
-            <Image src={lucas_full_body} rounded fluid alt='Picture of Lucas Hanson' style={{ backfaceVisibility: 'hidden', position: 'absolute', boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.5)' }}/>
-            <Image src={wet_lucas} rounded fluid alt='Picture of Lucas Hanson' style={{ backfaceVisibility: 'hidden', transform: 'rotateY(180deg)', position: 'absolute', boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.5)' }}/>
+            <Image src={lucas_full_body} rounded fluid alt='Formal picture of Lucas Hanson' style={{ backfaceVisibility: 'hidden', position: 'absolute', boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.5)' }}/>
+            <Image src={wet_lucas} rounded fluid alt='Picture of Lucas Hanson who has just dipped into the ocean' style={{ backfaceVisibility: 'hidden', transform: 'rotateY(180deg)', position: 'absolute', boxShadow: '0px 10px 30px rgba(0, 0, 0, 0.5)' }}/>
             <div style={{ paddingTop: '100%', visibility: 'hidden' }}></div>
           </div>
           <h1>&gt; Lucas Hanson</h1>
           <p>
           Aspiring software developer focused on backend, eager to improve frontend skills.<br /><br />
-          Check out my projects below.
+          Check out my <ScrollLink activeClass='active' to='projects' spy={true} duration={200} className='section-link' aria-label='Projects'>projects</ScrollLink> below.
           </p>
           </Container>
       </Row>
