18ed464ac8e169006e985f58015cae4b4b3e1006
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-07-08 21:07:52 +0200

    Try fix routing

--- lucas-website/src/index.js
+++ lucas-website/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log("render")
 root.render(
   <React.StrictMode>
-    <App />
+    <Router basename='/'>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
